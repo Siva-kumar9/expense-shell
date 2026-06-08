707802568b08a3405af771191a7f6489c33e74ac
Siva-kumar9 <sivalucky1909@gmail.com>
2026-06-08 13:20:03 +0530

    Expense Shell

--- mysql.sh
+++ mysql.sh
@@ -11,7 +11,7 @@ CHECKROOT()
     fi
 }
 
-LOG_FOLDER="/var/log/shellscript-logs"
+LOG_FOLDER="/var/log/expense-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
